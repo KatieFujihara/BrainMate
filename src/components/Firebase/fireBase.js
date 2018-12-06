92f301b62d29dea6856d015cc8be808272831328
katleiahramos <katleiahramos@gmail.com>
2018-12-06 14:15:41 -0600

    update credentials

--- src/components/Firebase/fireBase.js
+++ src/components/Firebase/fireBase.js
@@ -2,15 +2,19 @@ import app from 'firebase/app'
 import 'firebase/auth';
 
 
+  // TODO: environment variables not loading correctly
+  // const config = {
+  //   apiKey: process.env.REACT_APP_apikey,
+  //   authDomain: process.env.REACT_APP_authDomain,
+  //   databaseURL: process.env.REACT_APP_databaseURL,
+  //   projectId: process.env.REACT_APP_projectId,
+  //   storageBucket: process.env.REACT_APP_storageBucket,
+  //   messagingSenderId: process.env.REACT_APP_messagingSenderId
+  // };
 
   const config = {
-    apiKey: process.env.REACT_APP_apikey,
-    authDomain: process.env.REACT_APP_authDomain,
-    databaseURL: process.env.REACT_APP_databaseURL,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId
-  };
+
+  }
 
   class Firebase {
       constructor(){
@@ -25,6 +29,13 @@ import 'firebase/auth';
 
     doSignInWithEmailAndPassword = (email, password) =>
       this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
   }
 
   export default Firebase;
