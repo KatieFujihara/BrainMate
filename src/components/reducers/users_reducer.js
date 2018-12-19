020375ce1c5f8168e99169e20e1236c6de2fbb9f
katleiahramos <katleiahramos@gmail.com>
2018-12-18 18:03:22 -0600

    store saves userId after submitting registration form

--- src/components/reducers/users_reducer.js
+++ src/components/reducers/users_reducer.js
@@ -9,8 +9,7 @@ export default function userReducer(
         case 'BEGIN_USER_REQUEST':
             return state; 
         case 'LOGIN':
-            debugger
-            return { loggedIn: true, currentUserId: action.payload.userId }
+            return { loggedIn: true, currentUserId: action.userId }
         case 'LOGOUT':
             console.log("in LOGOUT returning", { ...state, loggedIn: true });
             return { ...state, loggedIn: true }
